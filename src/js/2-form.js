525de2575c0779ccaed58b5c6c8ffe40d0ac6ad4
hurzan.karina <kgurzan@gmail.com>
2025-02-19 13:05:38 +0000

    finish

---
+++ src/js/2-form.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const form = document.querySelector('.form');
+
+const localStorageKey = 'feedback-form-state';
+
+let formData = { email: '', message: '' };
+
+const savedData = localStorage.getItem(localStorageKey);
+if (savedData) {
+  formData = JSON.parse(savedData);
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+}
+
+form.addEventListener('input', event => {
+  formData[event.target.name] = event.target.value;
+
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (!formData.email.trim() || !formData.message.trim()) {
+    alert('Fill please all fields');
+    return;
+  } else {
+    console.log(formData);
+
+    localStorage.removeItem(localStorageKey);
+    formData = { email: '', message: '' };
+    form.reset();
+  }
+});
